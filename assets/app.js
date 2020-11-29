815dfc2d650f6855588e14d80c592278af4fee9f
Volmarg <dwlodarczyk12@gmail.com>
2020-11-29 13:21:13 +0100

    Further splitting of the template, add translations file

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,18 @@ import './styles/volt.scss';
 
 // todo: check how to add this @ in front and use this also in VUE when importing something in classes?
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'popper.js/dist/umd/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'onscreen/dist/on-screen.umd.js'
+import "nouislider/distribute/nouislider.min.js";
+import "jarallax/dist/jarallax.min.js";
+import "smooth-scroll/dist/smooth-scroll.polyfills.min.js";
+import "countup.js/dist/countUp.umd.js";
+import "notyf/notyf.min.js";
+import "chartist/dist/chartist.min.js";
+import "chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js";
+import "vanillajs-datepicker/dist/js/datepicker.min.js";
+import "simplebar/dist/simplebar.min.js";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
---
+++ translations/readme.md
@@ -0,0 +1,4 @@
+# Info
+## Translations structure
+
+Keep each file in separated folder - symfony will handle this without problem and this keeps the structure clear
