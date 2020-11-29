0860f4b8759f256f391c595b693876ea69510068
Volmarg <dwlodarczyk12@gmail.com>
2020-11-29 12:22:39 +0100

    Cut template into smaller parts, leave only few elments to work with

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/volt.scss';
+
+// todo: check how to add this @ in front and use this also in VUE when importing something in classes?
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
--- src/Action/TestAction.php
+++ src/Action/TestAction.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Action;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestAction
+class TestAction extends AbstractController
 {
     #[Route('/test' , name: 'test', methods: ["GET"])]
     public function testView()
     {
 
-        return new Response("nope");
+        return $this->render('base.twig');
     }
 
 }
